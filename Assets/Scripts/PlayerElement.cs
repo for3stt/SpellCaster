ffe660173be428ea7076d0524df2748e9a917c05
Ashish Bhattarai <ashishbhattarai@protonmail.com>
2023-11-06 10:44:45 +0700

    Add proper setter for player element

--- Assets/Scripts/PlayerElement.cs
+++ Assets/Scripts/PlayerElement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+using Unity.VisualScripting;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
@@ -14,18 +15,29 @@ public enum PlayerElementType
 public class PlayerElement : MonoBehaviour
 {
     private SpriteRenderer SpriteRenderer;
-    [field: SerializeField]
-    public PlayerElementType ElementType { get; private set; }
+    [SerializeField]
+    private PlayerElementType _elementType;
+    
+    public PlayerElementType ElementType
+    {
+        get { return this._elementType; }
+        set
+        {
+            this._elementType = value;
+            this.SpriteRenderer.color = GetElementColor(this._elementType);
+        }
+    }
 
     void Awake()
     {
         this.SpriteRenderer = GetComponent<SpriteRenderer>();
+        this.ElementType = this._elementType;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        this.SpriteRenderer.color = InitElementColor(this.ElementType);
+        //this.SpriteRenderer.color = GetElementColor(this.ElementType);
     }
 
     // Update is called once per frame
@@ -34,7 +46,7 @@ public class PlayerElement : MonoBehaviour
 
     }
 
-    static private Color InitElementColor(in PlayerElementType elementType)
+    static private Color GetElementColor(in PlayerElementType elementType)
     => elementType switch
     {
         PlayerElementType.Logi => new Color(0.8f, 0.3f, 0.1f),
