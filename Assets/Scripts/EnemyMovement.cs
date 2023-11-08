d90fb091abd6f297bfe0d70fd0cac84cc850ad26
for3stt <46494961+for3stt@users.noreply.github.com>
2023-11-08 15:32:22 -0800

    enemy follows player

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    Rigidbody body;
+    GameObject player;
+    Rigidbody playerBody;
     
     // Start is called before the first frame update
     void Awake()
     {
+        body = GetComponent<Rigidbody>();
+
+        player = GameObject.FindWithTag("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        if (player)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            if (distance > 1f)
+            {
+                float speed = moveSpeed * Time.deltaTime;
+                body.velocity = new Vector3(direction.x, direction.y, direction.z) * speed;
+            }
+            
+        }
     }
 }
