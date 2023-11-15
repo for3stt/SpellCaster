dc495d382cf762803a91759cf28341748ab51658
for3stt <46494961+for3stt@users.noreply.github.com>
2023-11-15 13:40:10 -0800

    cc

---
+++ Assets/Scripts/PlayerSpellCasting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpellCasting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
